d9b947818029db2fb4b94eb832f8a8764fd093f4
Lgotwals <lgotwals@purdue.edu>
2015-04-27 19:38:48 -0400

    Create game_main.c
    Main file for the game, implements functions.

---
+++ game_main.c
@@ -0,0 +1,20 @@
+/** game_main.c
+*
+*
+* Name:Lucy Gotwals, Luella Stonecipher, Sabrina Hsu, Katie Sigurdson, Patrick Hess
+* Course: CNIT 315
+* Group Project
+* Date:4/27/15
+*
+*
+*/
+
+int main()
+{
+  //implement game
+  
+  return 0;
+
+
+}
+
